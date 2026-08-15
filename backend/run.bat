5521cd1ae27b9d27f85e2dbb141d484c4304e0fa
Sundar S <sundarcompstudent@gmail.com>
2026-08-15 20:14:06 +0530

    Delete backend directory

--- backend/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.19.10-hotspot"
-set "PATH=%JAVA_HOME%\bin;C:\apache-maven-3.9.16\bin;%PATH%"
-cd /d "d:\My Portfolio Antigravity Project\backend"
-echo Using Java: %JAVA_HOME%
-java -version
-echo.
-echo Starting Spring Boot...
-mvn spring-boot:run
--- backend/src/main/java/com/portfolio/repository/ExperienceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portfolio.repository;
-
-import com.portfolio.model.Experience;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExperienceRepository extends JpaRepository<Experience, Long> {
-}
